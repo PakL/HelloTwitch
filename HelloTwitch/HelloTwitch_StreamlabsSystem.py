09761086cd0f308a1ff2eabbfbc0bb613eb70cd5
PakL <webmaster@paklweb.de>
2018-07-12 18:53:08 +0200

    Added an option for auto clearing the list after stream goes offline Also made some changes to make it easier to debug filter

--- HelloTwitch/HelloTwitch_StreamlabsSystem.py
+++ HelloTwitch/HelloTwitch_StreamlabsSystem.py
@@ -9,12 +9,13 @@ ScriptName = "HelloTwitch"
 Website = "https://github.com/PakL/HelloTwitch"
 Description = "Will give you a list of viewers that said hello (Please right-click + Insert API key)"
 Creator = "PakL"
-Version = "0.1.0"
+Version = "0.1.1"
 
 ht_phrases = []
 ht_userFilter = []
 ht_users = []
 ht_usersListed = {}
+ht_wasOnline = False
 
 def SetupSettings(data):
 	global ht_phrases, ht_userFilter
@@ -45,31 +46,40 @@ def Init():
 def Execute(data):
 	if len(data.User) > 0 and data.IsChatMessage() and data.IsFromTwitch():
 		if not data.IsWhisper():
-			doesMatch = False
-			doFilter = False
+			onMessage(data)
 
-			if len(''.join(ht_phrases)) <= 0:
-				doesMatch = True
-			else:
-				for index in range(len(ht_phrases)):
-					match = re.search("(^| )" + re.escape(ht_phrases[index]) + "( |$)", data.Message, re.IGNORECASE)
-					if match:
-						doesMatch = True
-
-			if len(''.join(ht_userFilter)) > 0:
-				for index in range(len(ht_userFilter)):
-					if data.User.lower() == ht_userFilter[index].lower():
-						doFilter = True
-			
-			if doesMatch and not doFilter:
-				Parent.Log(ScriptName, '[' + strftime("%H:%M:%S", localtime()) + "] Found matching message from " + data.User + ': ' + data.Message)
-				AppendUserToList(data.User, data.UserName)
-
+	if ht_wasOnline and not Parent.IsLive():
+		clearUserList()
+	ht_wasOnline = Parent.IsLive()
 	Parent.BroadcastWsEvent("HELLO_TWITCH_GREETING", json.dumps(ht_users))
 	return
 
 def Tick():
 	return
+
+def onMessage(data):
+	doesMatch = False
+	doFilter = False
+
+	if len(''.join(ht_phrases)) <= 0:
+		doesMatch = True
+	else:
+		for index in range(len(ht_phrases)):
+			match = re.search("(^| )" + re.escape(ht_phrases[index]) + "( |$)", data.Message, re.IGNORECASE)
+			if match:
+				doesMatch = True
+
+	if len(''.join(ht_userFilter)) > 0:
+		for index in range(len(ht_userFilter)):
+			if data.User.lower() == ht_userFilter[index].lower():
+				doFilter = True
+	
+	if doesMatch and not doFilter:
+		Parent.Log(ScriptName, '[' + strftime("%H:%M:%S", localtime()) + "] Found matching message from " + data.User + ': ' + data.Message)
+		AppendUserToList(data.User, data.UserName)
+
+	return
+
 
 def AppendUserToList(user, username):
 	global ht_users, ht_usersListed
@@ -100,5 +110,14 @@ def clearUserList():
 	global ht_users, ht_usersListed
 	ht_users = []
 	ht_usersListed = {}
+	Parent.BroadcastWsEvent("HELLO_TWITCH_GREETING", json.dumps(ht_users))
+	return
+
+def debugMessages():
+	onMessage(type('',(object,),{"User": "uSera", "UserName": "UserA", "Message": "Hello"})())
+	onMessage(type('',(object,),{"User": "usErb", "UserName": "UserB", "Message": "Hi"})())
+	onMessage(type('',(object,),{"User": "userc", "UserName": "UserC", "Message": "Hey"})())
+	onMessage(type('',(object,),{"User": "userd", "UserName": "UserD", "Message": "What's up?"})())
+
 	Parent.BroadcastWsEvent("HELLO_TWITCH_GREETING", json.dumps(ht_users))
 	return
--- README.md
+++ README.md
@@ -5,9 +5,12 @@ This makes it easier to greet your viewer. You can optionally specify phrases th
 
 In the settings you will also find a button to open the list as an html page that will connect to Streamlabs Chatbot and retrieve the list. (Otherwise you wouldn't be able to see it.)
 
-[Download](https://github.com/PakL/HelloTwitch/archive/master.zip) (0.1.0)
+[Download](https://github.com/PakL/HelloTwitch/archive/master.zip) (0.1.1)
 
 ## Changelog
+
+### (0.1.1) 2018-07-12
+* Added an option for auto clearing the list after stream goes offline
 
 ### (0.1.0) 2018-05-23
 * Added an option to exclude users from the list
